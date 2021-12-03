92f2a0d10725629c2d65572ba12a19d9ff0ddf88
ivanivnanov13 <45638263+ivanivnanov13@users.noreply.github.com>
2021-12-03 11:14:17 +0200

    Google maps.

--- src/cln/Client.java
+++ src/cln/Client.java
@@ -20,15 +20,17 @@ public class Client {
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		Socket server = new Socket("localhost", 1234);
-
+		String lng,lat;
 		BufferedReader input = new BufferedReader(new InputStreamReader(server.getInputStream()));
 
 		try {
 			System.out.println(input.readLine());
 			System.out.println("Location: " + input.readLine());
 			System.out.println("Tempreture: " + input.readLine() + "C");
+			lat=input.readLine();lng=input.readLine();
 			Desktop desktop = Desktop.getDesktop();
-			desktop.browse(new URI("https://www.google.com/maps/search/google+maps/@"+input.readLine()+ ","+input.readLine()+",15z"));
+		
+			desktop.browse(new URI("https://www.google.com/maps/search/?api=1&query="+lat+","+lng));
 			
 			if (input != null) {
 				input.close();
